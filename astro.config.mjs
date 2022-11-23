af42be26afbc2b2f678bff53268d9472e5ffbe26
amsiii <50972985+amsIII@users.noreply.github.com>
2022-11-23 08:15:45 -0500

    Working through blog post layout and header styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap"; // STORYBLOKS
 
-import sitemap from '@astrojs/sitemap';
+import storyblok from "@storyblok/astro";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), storyblok({
+    accessToken: "Khrfsg4VNrF3P1dssbjvPgtt",
+    bridge: true,
+    apiOptions: {
+      region: "us"
+    },
+    // storyblok-js-client options
+    components: {
+      page: "storyblok/Page",
+      feature: "storyblok/Feature",
+      grid: "storyblok/Grid",
+      teaser: "storyblok/Teaser"
+    }
+  }), tailwind()]
 });
